eb6331dd47062c13ad842eea2cb03b119e1b353d
rr.open.projects <ricardoribeiro.rr@outlook.com>
2021-04-01 18:17:09 +0100

    Create UploadPreview_img.js

---
+++ Javascript/WebSnippets/UploadPreview_img.js
@@ -0,0 +1,18 @@
+//*************************************************************//
+// HTML:
+//<input type='file' onchange="readURL(this);" />
+//<img id="blah" src="http://placehold.it/180" alt="your image" />
+//*************************************************************//
+
+function readURL(input) {
+            if (input.files && input.files[0]) {
+                var reader = new FileReader();
+
+                reader.onload = function (e) {
+                    $('#blah')
+                        .attr('src', e.target.result);
+                };
+
+                reader.readAsDataURL(input.files[0]);
+            }
+        }
